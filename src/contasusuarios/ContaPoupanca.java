b73a024d898d413a1ed1e5c993332caea906d885
Keyde-lab <keyde.silva@ueg.br>
2024-12-10 15:16:13 -0300

    Update ContaPoupanca.java

--- src/contasusuarios/ContaPoupanca.java
+++ src/contasusuarios/ContaPoupanca.java
@@ -11,7 +11,7 @@ public class ContaPoupanca extends Conta {
         super(saldo, titular, numeroConta, senha, "poupanca");
     }
 
-    //SACAR DA CONTA CORRENTE, ALEM DO SALDO ELA USA  UM CHEQUE ESPECIAL QUE SOMADO COM SALDO PERMITE O SAQUE
+    //SACAR DA CONTA CORRENTE. O SAQUE É FEITO APENAS CONSIDERANDO O SALDO DA CONTA.
     @Override
     public  void sacar(double valor) throws IOException {
         double saldo = getSaldo();
